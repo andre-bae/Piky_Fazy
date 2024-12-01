b707842feea79a5e7f4f8ef91c7fa26b9adeffc2
andre-bae <andre.bae@gmail.com>
2024-12-01 12:43:21 +0300

    Добавлено основное окно игры.

--- Piky_Fazy.py
+++ Piky_Fazy.py
@@ -104,76 +104,96 @@ else:
     num = 5
 # ----------------------------------------------------------------
 print(slognost, num)
-'''
-print(f'Это игра «Пики и фазы» («Быки и коровы»).\n' 
-      'Требуется угадать случайное четырехзначное число.\n'
-      'Вводятся пробные цифры.\n'\
-      'Если цифра по значению и позиции совпала с цифрой в исходном числе,\n'
-      'то эта цифра – «пика» («корова»).\n'
-      'Если же цифра не совпала по позиции с цифрой в исходном числе,\n'
-      'то эта цифра – «фаза» («бык»).\n'
-      'Например, если загадано число 1294, а названо число 1429,\n'
-      'то это одна «пика» и три «фазы»')
-print(f'\nВведите уровень сложности игры\n'
-      '0 - самый легкий. загадывается 5 цифр и они не повторяются в исходном числе\n'
-      '1 - Средний. Загадывается от 3 до 6 не повторяющихся цифр\n'
-      '2 - Сложный. Загадывается от 3 до 6 цифр, которые могут повторяться\n'
-      '( чем меньше цифр в загаданном числе - тем сложнее игра.\n')
-slognost = 3
-while slognost > 2:
-    N = input('Введите уровень сложности (от 0 до 2)?' )
-    if len(N)!=1 or not N.isdigit():        #Если введена не цифра от 0 до 9
-        N = input('Введите уровень сложности (от 0 до 2)?' )
-    slognost= int(N)
 
-num = 0                                     #Количество загаданных цифр
-if slognost==0:
-    num = 5
-else:
-    while num<3 or num>6:
-        N = input('сколько цифр будем отгадывать (от 3 до 6)?')
-        if len(N)!=1 or not N.isdigit():    #Если введена не цифра от 0 до 9
-            N = input('сколько цифр будем отгадывать (от 3 до 6)? ')
-        num = int(N)
-zagadano = str(random.randint(0,9))         #Загадываем заданное число цифр   
-for i in range(1,num):
-    fig = str(random.randint(0,9))
+zagadano = str(random.randint(0, 9))  # Загадываем заданное число цифр
+for i in range(1, num):
+    fig = str(random.randint(0, 9))
     if slognost != 2:
-        while fig in zagadano:              #Исключаем повторяющеся цифры
-            fig = str(random.randint(0,9))
+        while fig in zagadano:  # Исключаем повторяющеся цифры
+            fig = str(random.randint(0, 9))
     zagadano += fig
 #    print(zagadano)
+# zagadano='02763'
 
-#zagadano='02763'
+
+'''
+main_window = Tk()
+main_window.title("Быки и коровы")
+#main_window.resizable(False, False)
+main_window.attributes("-toolwindow", True)
+main_window.geometry("250x200+400+200")
+
+frame_ask = Frame(main_window)
+frame_ask.pack(side=LEFT)
+
+def read_popytka():
+    global popytka
+    popytka = e.get()
+
+invite = ttk.Label(frame_ask, text=f'Введите {num} цифр', justify="left",
+                   bg="gray", fg="white", font="Courier 18 bold")
+invite.pack(side=TOP)
+e = Entry(frame_ask, width=num, justify="left", bg="gray", fg="white",
+          font="Courier 18 bold")
+e.pack(side=LEFT)
+b = Button(frame_ask, text="Ввод", bg="grey",
+           font="Courier 12 bold", command=read_popytka)
+b.pack(side=LEFT)
+main_window.mainloop()
+'''
+
 skolko = 0
-while True:                           
+while True:
     f = 0
     p = 0
-    pika = [0]*num
-    faza = [0]*num
-    popytka = input(f'Введите {num} цифр ')
+    pika = [0] * num
+    faza = [0] * num
+    #    popytka = input(f'Введите {num} цифр ')
+    main_window = Tk()
+    main_window.title("Быки и коровы")
+    # main_window.resizable(False, False)
+    main_window.attributes("-toolwindow", True)
+    main_window.geometry("250x200+400+200")
 
-    if popytka=='':                         #завершение игры - ввод пустой строки
-        print(f'Жаль! Всего {skolko} попыток! Было загадано {zagadano}')
+    frame_ask = Frame(main_window)
+    frame_ask.pack(side=LEFT)
+
+
+    def read_popytka():
+        global popytka
+        popytka = e.get()
+
+
+    invite = Label(frame_ask, text=f'Введите {num} цифр', justify="left",
+                   bg="gray", fg="white", font="Courier 18 bold")
+    invite.pack(side=TOP)
+    e = Entry(frame_ask, width=num, justify="left", bg="gray", fg="white",
+              font="Courier 18 bold")
+    e.pack(side=LEFT)
+    b = Button(frame_ask, text="Ввод", bg="grey",
+               font="Courier 12 bold", command=read_popytka)
+    b.pack(side=LEFT)
+
+    if popytka == '':  # завершение игры - ввод пустой строки
+        invite['text'] = f'Жаль! Всего {skolko} попыток! Было загадано {zagadano}'
         break
-    while len(popytka)!=num or not popytka.isdigit():  #Если введено не num цифр
+    while len(popytka) != num or not popytka.isdigit():  # Если введено не num цифр
         popytka = input(f'Введите {num} цифр ')
-    for i in range(num):                    #Подсчет угаданных цифр на своем месте (Быков)
-        pika[i] = (popytka[i]==zagadano[i])
-    for i in range(num):                    #Подсчет угаданных цифр не на своем месте (коров)
+    for i in range(num):  # Подсчет угаданных цифр на своем месте (Быков)
+        pika[i] = (popytka[i] == zagadano[i])
+    for i in range(num):  # Подсчет угаданных цифр не на своем месте (коров)
         if not pika[i]:
             for j in range(num):
-                faza[i] += (not pika[j]) and (popytka[j]==zagadano[i])
-#               print(i,j,pika,faza)
-        if faza[i]:                         # Всего "быков" и "коров" в попытке
+                faza[i] += (not pika[j]) and (popytka[j] == zagadano[i])
+        #               print(i,j,pika,faza)
+        if faza[i]:  # Всего "быков" и "коров" в попытке
             faza[i] = 1
         f += faza[i]
         p += pika[i]
     print(f'Быков {p}, коров {f}')
-    skolko += 1                             # Количество сделанных попыток
-    if popytka==zagadano:
-        print(f'Вы угадали за {skolko} попыток! Это {zagadano}')
+    skolko += 1  # Количество сделанных попыток
+    if popytka == zagadano:
+        invite['text'] = f'Вы угадали за {skolko} попыток! Это {zagadano}'
         break
-'''
-# okno.mainloop()
-# help.mainloop()
+
+main_window.mainloop()
